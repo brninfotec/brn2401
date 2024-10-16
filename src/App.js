3b743d6babff4907cb416b400abfbbd74273e1dd
unknown <brninfotec>
2024-10-16 13:43:27 +0530

    add some headings in app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>2406 batch</h1>
+      <h1>2407 batch</h1>
+      <h1>2408 batch</h1>
+      <h1>2409 batch</h1>
+    </div>
+  );
+}
+
+export default App;
